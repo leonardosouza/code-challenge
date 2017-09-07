c6bb99a77ba3e3f2179719dce9428ccec9ebba54
Leonardo Souza <leonardodisouza@gmail.com>
2017-09-07 00:30:16 -0300

    configuring webpack

---
+++ src/js/cssFiles.js
@@ -0,0 +1,7 @@
+import '../css/base/reset.css'
+import '../css/base/typography.css'
+import '../css/base/layout.css'
+
+import '../css/components/search.css'
+import '../css/components/best-experience.css'
+import '../css/components/product.css'
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const path = require('path')
+const webpack = require('webpack')
+const HtmlPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CleanPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: path.join(__dirname, 'src', 'js', 'index'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]-[hash].js',
+    publicPath: ''
+  },
+  plugins: [
+    new CleanPlugin(['dist']),
+    // new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('[name]-[hash].css'),
+    new HtmlPlugin({
+      title: 'Beer Delivery',
+      template: path.join(__dirname, 'src', 'html', 'template.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: [{ loader: 'standard-loader' }]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: [{ loader: 'babel-loader' }]
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      src: path.join(__dirname, 'src')
+    }
+  }
+}
